c564f3e88eaf304e0dba99bc6f62d0b3cd1e0dc1
Manjunath-PC\Manjunath <manjunath.vernekar@bookmyshow.com>
2018-07-06 22:35:39 +0530

    Final

--- src/facebookMobile/UserProfilePage.java
+++ src/facebookMobile/UserProfilePage.java
@@ -5,13 +5,9 @@ import static org.testng.Assert.assertEquals;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 
@@ -36,8 +32,6 @@ public String message;
 public void verifyPost(String posttext) 
 {
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	//WebDriverWait wait=new WebDriverWait(driver, 20);
-	//postedMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.ViewGroup[[contains(@resource-id,'feed_story_message') and @index='5']")));
 	message = postedMessage.getAttribute("content-desc").toString();
 	log.debug("Verifu the message posted");
 	assertEquals(message, posttext, "comment not posted");
